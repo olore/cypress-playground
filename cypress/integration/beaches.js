1be2fb9c575fa6570791b095c69c97f416e78ce8
Brian Olore <brian@olore.net>
2018-09-03 21:21:53 -0400

    run through more dates

--- cypress/integration/beaches.js
+++ cypress/integration/beaches.js
@@ -2,20 +2,82 @@
 
 const { _, $ } = Cypress
 
+var outfile = '/Users/brian/dev/beaches' + '/output.log';
+
 context('Window', () => {
+
+  before(() => {
+    cy.exec(`echo \`date "+%Y-%m-%d"\` >> ${outfile}`)
+  })
 
   beforeEach(() => {
     cy.visit('https://obe.beaches.com/index.cfm?event=ehOBE.dspHome&checked=true')
+    cy.get('#resortcode').select('BNG,B,12'); // Negril
   })
 
-  it('do stuff', () => {
-
+  it('Easter 2019', () => {
     let day1 = Cypress.moment('2019-04-20');
     let day2 = Cypress.moment('2019-04-26');
-    let price = '$6,642.00';
+    setDates(day1, day2);
 
-    cy.get('#resortcode').select('BNG,B,12');
+    cy.get('#Child_1').select('15');
+    cy.get('#Child_2').select('12');
 
+    cy.get('#btnGetRates > a').click();
+
+    doit('Easter  2019');
+  });
+
+  it('NJ Week 2019', () => {
+    let day1 = Cypress.moment('2019-11-02');
+    let day2 = Cypress.moment('2019-11-08');
+    setDates(day1, day2);
+    cy.get('#Child_1').select('15');
+    cy.get('#Child_2').select('12');
+    cy.get('#btnGetRates > a').click();
+
+    doit('NJ Week 2019');
+  });
+
+  it('Easter 2020', () => {
+    let day1 = Cypress.moment('2020-04-11');
+    let day2 = Cypress.moment('2020-04-17');
+    setDates(day1, day2);
+    cy.get('#adults').select('3');
+    cy.get('#children').select('1');
+    cy.get('#Child_1').select('13');
+    cy.get('#btnGetRates > a').click();
+
+    doit('Easter 2020');
+  });
+
+  it('NJ Week 2020', () => {
+    let day1 = Cypress.moment('2020-10-31');
+    let day2 = Cypress.moment('2020-11-06');
+    setDates(day1, day2);
+    cy.get('#adults').select('3');
+    cy.get('#children').select('1');
+    cy.get('#Child_1').select('14');
+    cy.get('#btnGetRates > a').click();
+
+    doit('NJ Week 2020');
+  });
+
+  it('Easter 2021', () => {
+    let day1 = Cypress.moment('2021-03-27');
+    let day2 = Cypress.moment('2021-04-02');
+    setDates(day1, day2);
+    cy.get('#adults').select('3');
+    cy.get('#children').select('1');
+
+    cy.get('#Child_1').select('13');
+    cy.get('#btnGetRates > a').click();
+
+    doit('Easter  2021');
+  });
+
+
+  function setDates(day1, day2) {
     cy.get('#checkin_month').select(day1.format('M'));
     cy.get('#checkin_day').select(day1.format('D'));
     cy.get('#checkin_year').select(day1.format('YYYY'));
@@ -26,25 +88,22 @@ context('Window', () => {
 
     cy.get('#adults').select('2');
     cy.get('#children').select('2');
-    cy.get('#Child_1').select('15');
-    cy.get('#Child_2').select('12');
+  }
 
-    cy.get('#btnGetRates > a').click();
-
+  function doit(str) {
     cy.get('.colC', { timeout: 30000 }).then((cols) => {
       let vals = _.map(cols.get(), (col, i) => {
         let text = $(col).text();
         if (/\$/.test(text)) {
-          return text.trim().substr(2,10).trim();
+          text = text.trim().substr(2,10).replace(',', '').replace('$', '').trim();
+          return _.parseInt(text);
         }
       });
 
-      let v = _.compact(vals);
-      cy.log('- - - - --  -- -- - - -');
-      cy.log(v[0]);
-      expect(v[0]).to.equal(price);
+      let v = vals.sort((a, b) => a - b);
+      let output = str + ': \\$' + v[0];
+      cy.exec(`echo "${output}" >> ${outfile}`)
     });
-
-  });
+  }
 
 })
